3cf881fbf0b36b4412df40b37cce12701200ede2
YoshikazuHosono <yyhhwork@gmail.com>
2022-03-03 14:00:41 +0900

    navigation var

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,17 @@
 <template>
   <v-app>
+    <v-navigation-drawer app clipped v-model="drawer">
+      <v-divider />
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title> TEST </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar app elevate-on-scroll>
-      <v-app-bar-nav-icon />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn v-if="!$store.state.isLogin" @click="login">LOGIN</v-btn>
@@ -20,6 +30,7 @@ export default {
   data() {
     return {
       title: "Hosono.js",
+      drawer: false,
     };
   },
   methods: {
