b3fbe6587a3b177de580d5d034b739717449da9d
YoshikazuHosono <yyhhwork@gmail.com>
2022-03-03 13:06:10 +0900

    v-app

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,15 @@
 <template>
-  <v-main>
-    <v-container>
-      <Nuxt />
-    </v-container>
-  </v-main>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: "DefaultLayout",
   data() {
     return {
       title: "Hosono.js",
